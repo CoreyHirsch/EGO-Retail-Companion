c8b00326852307653f9c1882ab0a04ea05cebfb9
Corey Hirsch <s0522777@student.rockvalleycollege.edu>
2022-10-23 21:01:02 -0500

    Initial Commit

---
+++ EGO Retail Companion/EGO_Retail_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGO_Retail_CompanionApp.swift
+//  EGO Retail Companion
+//
+//  Created by Corey Hirsch on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct EGO_Retail_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
